4acda4f6df00b49ddbe8874263762c38d7b47128
PauloVicente89 <pauloviana089@gmail.com>
2025-01-22 13:16:32 -0300

    fix: permissions

--- project/modules/car/views/delete.py
+++ project/modules/car/views/delete.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from modules.car.services import CarService
+from rest_framework.permissions import IsAdminUser
 
 class DeleteCarsView(APIView):
+    permission_classes = [IsAdminUser]
+
     def delete(self, request, car_id: int):
         CarService.delete_car(car_id=car_id)
         return Response(status=status.HTTP_204_NO_CONTENT)
